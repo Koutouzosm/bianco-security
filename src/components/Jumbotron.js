9cde3b6a82f78c00e0bbbd80b27ff41961adf42d
Mano <Koutouzosm@gmail.com>
2019-06-27 10:47:09 -0400

    saving work

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return(
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Bianco Security</h1>
+    <p class="lead"></p>
+  </div>
+</div>
+    )
+}
+
+export default Jumbotron;
