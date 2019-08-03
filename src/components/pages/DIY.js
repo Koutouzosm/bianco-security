d14b098bcf42967c7c4916e6253e744421ecf11b
Mano <Koutouzosm@gmail.com>
2019-08-03 10:05:16 -0400

    added DIY page and Products page

---
+++ src/components/pages/DIY.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navi from './components/Navbar'
+
+
+class DIY extends Component {
+
+    render() {
+        return(
+            <React.Fragment>
+                <Navi />
+            </React.Fragment>
+        )
+    }
+}
+
+export default DIY;
---
+++ src/components/pages/Products.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navi from './components/Navbar'
+
+
+class Products extends Component {
+
+    render() {
+        return(
+            <React.Fragment>
+                <Navi />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Products;
